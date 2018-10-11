d59e9b144a28d06c5c1e17dacfca1ee843936c7e
Travis Nguyen <travishn@uci.edu>
2018-10-11 12:15:20 -0700

    Add app structure

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 6000;
+
+app.get('/test', (req, res) => {
+  res.send({
+    express: 'This is on the correct server'
+  })
+});
+
+app.listen(PORT, () => {
+  console.log(`Currently listening on port ${PORT}`)
+});
