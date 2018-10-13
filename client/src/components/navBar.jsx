0c61ddeedf70f454421474332c015ffca0401b04
Travis Nguyen <travishn@uci.edu>
2018-10-12 20:29:11 -0700

    Add logo to nav bar

--- client/src/components/navBar.jsx
+++ client/src/components/navBar.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 class NavBar extends React.Component {
+  renderLogo() {
+    return (
+      <div className='left-nav-child'>
+        <Link to='/' className="header-logo">
+          <img src='../../public/images/maple-logo-2' alt='home-logo'/>
+          <h1>WANDERCAMP</h1>
+        </Link>
+      </div>
+    );
+  }
+
   render() {
     return (
-      <section className='navbar-container'>
+      <nav className='navbar-container'>
         <p>Insert NavBar Here</p>
-      </section>
+      </nav>
     )
   }
 };
